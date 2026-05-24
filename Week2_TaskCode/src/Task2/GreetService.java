97c4cbe4bee8a25bd95a00061c129246d7ab0508
Lin12141125 <119798805+Lin12141125@users.noreply.github.com>
2026-05-24 10:09:35 +0800

    Delete Old Week2 TaskCode

--- Week2_TaskCode/src/Task2/GreetService.java
+++
@@ -1,8 +0,0 @@
-package Task2;
-
-@MyComponent
-public class GreetService {
-    public String getGreeting(){
-        return "<h1>Greetings from GreetingService</h1>";
-    }
-}
--- Week2_TaskCode/src/Task3/UserService.java
+++
@@ -1,6 +0,0 @@
-package Task3;
-
-public interface UserService {
-    void saveUser(String name);
-    String method2();
-}
