2845494f96bd753c5b97ef46c63f5efdbc11e47f
Lin12141125 <2546156509@qq.com>
2026-06-14 21:57:29 +0800

    update DTO

--- Week5/usermanagement/src/main/java/com/example/usermanagement/dto/RegisterDTO.java
+++ Week5/usermanagement/src/main/java/com/example/usermanagement/dto/RegisterDTO.java
@@ -1,17 +1,21 @@
 package com.example.usermanagement.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-/***
- * UserCreateDTO class: 新增用户 参数校验
- */
 @Data
-public class UserCreateDTO {
+public class RegisterDTO {
     @NotBlank(message = "用户名不能为空")
-    // 不能空不能Null 不能为 null、""、"   "
     @Size(min = 1, max = 50, message = "用户名长度必须在1到50之间")
     private String username;
+
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 6, max = 50, message = "密码长度至少6位")
+    private String password;
 
     @NotBlank(message = "邮箱不能为空")
     @Email(message = "邮箱格式不正确")
@@ -19,5 +23,5 @@ public class UserCreateDTO {
 
     @Min(value = 1, message = "年龄必须在1到150之间")
     @Max(value = 150, message = "年龄必须在1到150之间")
-    private  Integer age;
+    private Integer age;
 }
