ecf288753f7e1fa7773d6a9a7f9d1f862ad54b21
Lin12141125 <2546156509@qq.com>
2026-06-21 06:44:59 +0800

    docker set

--- Week6/usermanagement/src/test/java/com/example/usermanagement/UsermanagementApplicationTests.java
+++ Week6/usermanagement/src/test/java/com/example/usermanagement/UsermanagementApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.usermanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"JWT_SECRET=test-jwt-secret-please-change-in-production-123456"
+})
 class UsermanagementApplicationTests {
 
 	@Test
